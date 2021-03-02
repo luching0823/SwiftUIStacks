642cfa456099811a5b98a1cee94274fa0a7bccd5
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-02 13:52:56 +0800

    Initial Commit

---
+++ SwiftUIStacks/SwiftUIStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStacksApp.swift
+//  SwiftUIStacks
+//
+//  Created by 廖昱晴 on 2021/3/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
